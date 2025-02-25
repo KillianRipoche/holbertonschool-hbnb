d82b4d72628ebd8aca392a019a22e03cf4cae400
Albat93 <albat93120@gmail.com>
2025-02-25 10:52:45 +0100

    add module associate with place

--- part-2/app/models/amenity.py
+++ part-2/app/models/amenity.py
@@ -1,6 +1,14 @@
 from BaseModel import BaseModel
+from place import Place
+
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = name[:50]  # Limité à 50 caractères
         self.places = []  # Liste des endroits où cet équipement est disponible
+        self.amenities = []
+
+    def associate_with_place(self, place):
+        if place not in self.places:
+            self.places.append(Place)
+            self.amenities.append(Place)
