1c67fce2e0f72c921613b57248365657c13604ba
Albat93 <albat93120@gmail.com>
2025-03-11 15:57:14 +0100

    merge branch to fetch modifs from Killian

--- part-3/app/api/v1/admin.py
+++ part-3/app/api/v1/admin.py
@@ -1,6 +1,7 @@
 from flask_restx import Namespace, Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import request
+from functools import wraps
 from app import facade
 
 api = Namespace('admin', description='Admin operations')
