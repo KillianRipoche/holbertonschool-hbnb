06da607a00b0e87620645246e5a5159b36e0469b
Albat93 <albat93120@gmail.com>
2025-03-11 15:38:48 +0100

    add admin namespace and user management endpoints; implement user and amenity creation and modification

--- part-3/app/__init__.py
+++ part-3/app/__init__.py
@@ -7,6 +7,7 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.auth import api as auth_ns
+from app.api.v1.admin import api as admin_ns
 from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
@@ -25,6 +26,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(admin_ns, path='/api/v1/admin')
 
 
 
