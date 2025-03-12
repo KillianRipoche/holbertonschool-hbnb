7bd5214d94f4965dc053d83bada7f9165f3f0f01
KillianRipoche <killian.ripoche@gmail.com>
2025-03-12 10:56:20 +0100

    Add admin secret

--- part-3/config.py
+++ part-3/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    ADMIN_SECRET = os.getenv('ADMIN_SECRET', 'default_admin_secret')
     DEBUG = False
 
 
