a54d18cd7f4b92bbd46863edad754b8a8c7238fa
KillianRipoche <killian.ripoche@gmail.com>
2025-03-12 09:51:37 +0100

    Add the admin control in v1/amenities.py

--- part-3/app/api/v1/amenities.py
+++ part-3/app/api/v1/amenities.py
@@ -1,21 +1,26 @@
 from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.services import facade
 
 api = Namespace('amenities', description='Amenity operations')
 
-
 amenity_model = api.model('Amenity', {
     'name': fields.String(required=True, description='Name of the amenity')
 })
-
 
 @api.route('/')
 class AmenityList(Resource):
     @api.expect(amenity_model, validate=True)
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
+    @api.response(403, 'Admin privileges required')
+    @jwt_required()
     def post(self):
-        """Register a new amenity"""
+        """Register a new amenity (ADMIN ONLY)"""
+        current_user = get_jwt_identity()
+        if not current_user.get('is_admin'):
+            return {'error': 'Admin privileges required'}, 403
+
         amenity_data = api.payload
         try:
             new_amenity = facade.create_amenity(amenity_data)
@@ -57,12 +62,17 @@ class AmenityResource(Resource):
     @api.response(200, 'Amenity updated successfully')
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
+    @api.response(403, 'Admin privileges required')
+    @jwt_required()
     def put(self, amenity_id):
-        """Update an amenity's information"""
+        """Update an amenity's information (ADMIN ONLY)"""
+        current_user = get_jwt_identity()
+        if not current_user.get('is_admin'):
+            return {'error': 'Admin privileges required'}, 403
+
         amenity_data = api.payload
         try:
-            updated_amenity = facade.update_amenity(
-                amenity_id, amenity_data)
+            updated_amenity = facade.update_amenity(amenity_id, amenity_data)
             if not updated_amenity:
                 return {'error': 'Amenity not found'}, 404
             return {
