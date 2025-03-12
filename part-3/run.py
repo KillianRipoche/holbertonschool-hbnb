7b7f25f3489d538d15f7c55337e9b1e08af1ed9a
KillianRipoche <killian.ripoche@gmail.com>
2025-03-12 10:56:52 +0100

    Initial commit for run.py

--- part-3/run.py
+++ part-3/run.py
@@ -1,6 +1,43 @@
-from app import create_app
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.admin import api as admin_ns
+from app.api.v1.users import api as users_ns
+from config import config
+from flask import Flask
+from flask_restx import Api
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 
-app = create_app()
+# Load variables from .env file
+load_dotenv()
+
+
+# Import namespaces from your API modules
+
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app_config = config[config_name]
+    app.config.from_object(app_config)
+
+    # Initialize Flask-JWT-Extended
+    app.config['JWT_SECRET_KEY'] = app.config['SECRET_KEY']
+    jwt = JWTManager(app)
+
+    # Initialize Flask-Restx API
+    api = Api(app, version='1.0', title='Hbnb API', description='API for Hbnb')
+
+    # Register namespaces
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(admin_ns, path='/api/v1/admin')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    return app
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application = create_app('development')
+    application.run(host='0.0.0.0', port=5000)
