26b67d4e11556e3eb5a07e7f1fb3451df01167de
KillianRipoche <killian.ripoche@gmail.com>
2025-03-12 10:58:52 +0100

    add comm and admin

--- part-3/app/persistence/repository.py
+++ part-3/app/persistence/repository.py
@@ -1,56 +1,37 @@
-from abc import ABC, abstractmethod
+import uuid
 
 
-class Repository(ABC):
-    @abstractmethod
-    def add(self, obj):
-        pass
+class InMemoryRepository:
+    """
+    Simple in-memory repository for storing objects.
+    """
 
-    @abstractmethod
-    def get(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_all(self):
-        pass
-
-    @abstractmethod
-    def update(self, obj_id, data):
-        pass
-
-    @abstractmethod
-    def delete(self, obj_id):
-        pass
-
-    @abstractmethod
-    def get_by_attribute(self, attr_name, attr_value):
-        pass
-
-
-class InMemoryRepository(Repository):
     def __init__(self):
-        self._storage = {}
+        self.storage = {}
 
     def add(self, obj):
-        self._storage[obj.id] = obj
-
-    def get(self, obj_id):
-        return self._storage.get(obj_id)
-
-    def get_all(self):
-        return list(self._storage.values())
-
-    def update(self, obj_id, data):
-        obj = self.get(obj_id)
-        if obj:
-            obj.update(data)
+        if not obj.id:
+            obj.id = str(uuid.uuid4())
+        self.storage[obj.id] = obj
         return obj
 
+    def get(self, obj_id):
+        return self.storage.get(obj_id)
+
     def delete(self, obj_id):
-        return self._storage.pop(obj_id, None)
+        return self.storage.pop(obj_id, None)
+
+    def update(self, obj_id, new_obj):
+        if obj_id in self.storage:
+            self.storage[obj_id] = new_obj
+            return new_obj
+        return None
 
     def get_by_attribute(self, attr_name, attr_value):
-        for obj in self._storage.values():
-            if getattr(obj, attr_name, None) == attr_value:
+        for obj in self.storage.values():
+            if hasattr(obj, attr_name) and getattr(obj, attr_name) == attr_value:
                 return obj
         return None
+
+    def get_all(self):
+        return list(self.storage.values())
