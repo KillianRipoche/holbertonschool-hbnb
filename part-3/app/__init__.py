300e31bbf593e7f466b4efd8040f38646d041657
Albat93 <albat93120@gmail.com>
2025-03-12 12:24:11 +0100

    refacto init.py /app file

--- part-3/app/__init__.py
+++ part-3/app/__init__.py
@@ -7,8 +7,10 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -19,9 +21,11 @@ def create_app(config_class="config.DevelopmentConfig"):
     jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API')
+
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(auth_ns, path="/api/v1/auth")
     return app
